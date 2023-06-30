ca28dc123ff9745190f7a96ce4a9c04f02351e17
cobocho <rlaalsrb1111@naver.com>
2023-06-30 20:41:30 +0900

    feat: date picker 구현

--- src/pages/DaypickerPage.tsx
+++ src/pages/DaypickerPage.tsx
@@ -1,15 +1,55 @@
-import React from 'react';
 import { styled } from 'styled-components';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { Calendar, DateRange, RangeKeyDict } from 'react-date-range';
+import { useState } from 'react';
+import { Range } from 'react-date-range';
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  align-items: center;
   width: 800px;
-  min-height: 600px;
+  height: fit-content;
+  min-height: 300px;
+  padding: 40px 20px;
   border-radius: 20px;
   box-shadow: 0px 0px 22px -6px rgba(0, 0, 0, 0.2);
 `;
 
 const DaypickerPage = () => {
-  return <Container></Container>;
+  const [range, setRange] = useState<Range>({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+  });
+
+  function selectHandler(date: Date) {
+    console.log(date);
+  }
+
+  function rangeHandler(range: RangeKeyDict) {
+    setRange(range.selection);
+  }
+
+  return (
+    <Container>
+      <Calendar
+        date={new Date()}
+        onChange={selectHandler}
+      />
+      <DateRange
+        ranges={[range]}
+        rangeColors={['orange']}
+        editableDateInputs={true}
+        moveRangeOnFirstSelection={false}
+        onChange={rangeHandler}
+      />
+      <p>{range.startDate?.toDateString()}</p>
+      <p>{range.endDate?.toDateString()}</p>
+    </Container>
+  );
 };
 
 export default DaypickerPage;
